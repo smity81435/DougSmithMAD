100e96920254c5b0966f4dc4475353ffb124521d
smity81435 <smithdouglas25@gmail.com>
2018-12-13 10:18:01 -0700

    implicit button

--- projects/NORN/app/src/main/java/com/example/douglassmith/norn/Quest.java
+++ projects/NORN/app/src/main/java/com/example/douglassmith/norn/Quest.java
@@ -1,16 +1,36 @@
 package com.example.douglassmith.norn;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Quest extends AppCompatActivity {
+    private String url = "http://dpower3.wordpress.com";
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest);
 
+        final ImageButton blog = findViewById(R.id.blogbutton);
+        View.OnClickListener clicked = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadWebSite(v);
+            }
+        };
+        blog.setOnClickListener(clicked);
 
 
+    }
+
+    public void loadWebSite(View view){
+        //implicit intent
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 
 }
