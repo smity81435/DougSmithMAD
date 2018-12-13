62b484dc2c04cf0e8afefba838fcf2f65db5a297
smity81435 <smithdouglas25@gmail.com>
2018-12-12 18:27:20 -0700

    NORN init in projects

---
+++ projects/NORN/app/src/main/java/com/example/douglassmith/norn/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.douglassmith.norn;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SimpleDateFormat formatter
+                = new SimpleDateFormat ("EEE, MMM d, ''yy" );
+        Date currentTime_1 = new Date();
+        String dateString = formatter.format(currentTime_1);
+        TextView usedDate = (TextView) findViewById(R.id.currentDate);
+        usedDate.setText(dateString);
+    }
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(FromActivity.this, ToActivity.class);
+        startActivity(intent);
+    }
+}
+
